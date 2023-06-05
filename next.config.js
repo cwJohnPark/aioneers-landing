2dbff407e4b9717cf0d1039a39dceb74c301753f
cwjohnpark <pierrotcw@gmail.com>
2023-06-05 19:23:24 +0900

    fix: next js static export config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/aioneers-landing",
+  output: "export",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
